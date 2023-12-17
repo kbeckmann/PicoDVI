336f21335ec73ae5c3a9a04bd582a1ac841f7f18
Konrad Beckmann <konrad.beckmann@gmail.com>
2023-12-17 14:43:20 +0100

    Add a few comments

--- software/apps/n64/osd.h
+++ software/apps/n64/osd.h
@@ -8,7 +8,15 @@
 
 #include "joybus.h"
 
-// Key combination to enter the OSD menu (L + R + C-DOWN + DPAD-DOWN)
+/**
+ * @brief Key combination to enter the OSD menu.
+ *
+ * This macro checks if the specified key combination is pressed.
+ * The key combination for the OSD menu is L + R + C-DOWN + DPAD-DOWN.
+ *
+ * @param __keys__ The current state of the keys.
+ * @return True if the OSD shortcut keys are pressed, false otherwise.
+ */
 #define OSD_SHORTCUT(__keys__) ( \
     TL_BUTTON(__keys__) && \
     TR_BUTTON(__keys__) && \
@@ -16,4 +24,9 @@
     DD_BUTTON(__keys__)    \
 )
 
+/**
+ * @brief Run the OSD.
+ *
+ * This function runs the OSD. It should be called in your main loop.
+ */
 void osd_run(void);
